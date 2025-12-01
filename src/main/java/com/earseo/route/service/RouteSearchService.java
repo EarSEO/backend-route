167eab96cf1f113631f8bf3e449cafec1ba148f6
haribonyam <drppr99@gmail.com>
2025-12-02 05:40:16 +0900

    [HOTFIX] fix: 위도 경도 좌표 변경

--- src/main/java/com/earseo/route/service/RouteSearchService.java
+++ src/main/java/com/earseo/route/service/RouteSearchService.java
@@ -101,7 +101,7 @@ public class RouteSearchService {
                 double lon = points.getLon(p);
 
                 pointRequests.add(new PointRequest(lat, lon));
-                routes.add(new RoutePathPoint(lat, lon));
+                routes.add(new RoutePathPoint(lon, lat));
 
                 allCoords.add(new Coordinate(lon, lat));
             }
